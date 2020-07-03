5582f10cb97b733fcb4dc6c1eb44201bec4fa90e
donpapitoskey <alarcon.juan@correounivalle.edu.co>
2020-07-02 21:17:06 -0500

    Comments added for readability of the file

--- cachematrix.R
+++ cachematrix.R
@@ -3,14 +3,18 @@
 
 ## Write a short comment describing this function
 
+# makeCacheMatrix
+# INPUTS: void
+#OUTPUT: list assigned to variable with different inner functions
 makeCacheMatrix <- function(x = matrix()) {
 
- 
+    #defining inner functions for definition of the variable
     inv <- NULL
-    set <- function (y){
+    set <- function (y){  
       x<<- y
       inv <<- NULL
     }
+    #defining the inner functions for opperations in the variable
     get <- function() x
     setInv <- function(solve) inv <<- solve
     getInv <- function() inv
@@ -24,11 +28,13 @@ makeCacheMatrix <- function(x = matrix()) {
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
+  #defining funcitons for variable fileds write-reading
   inv<-x$getInv()
   if(!is.null(inv)){
     message("getting cached data")
     return(inv)
   }
+  #defining functions for variable fields operation
   data<-x$get()
   inv <-solve(data,..)
   x$setInv(inv)
